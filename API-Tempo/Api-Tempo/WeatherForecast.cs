ba8c4575cf19d22f0dfe799f0a1ff456290f5047
Juarez Goulart <37718377+Goulart12@users.noreply.github.com>
2025-09-21 17:40:37 -0300

    Add files via upload

---
+++ API-Tempo/Api-Tempo/WeatherForecast.cs
@@ -0,0 +1,14 @@
+namespace Api_Tempo;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public int TemperatureK => TemperatureC + 273;
+
+    public string? Summary { get; set; }
+}
